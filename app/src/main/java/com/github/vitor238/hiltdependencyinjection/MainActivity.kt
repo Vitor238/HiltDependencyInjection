7d1325ff2d37f2556d1a3f091d0f1f738cfad2c0
Vitor Ferreira <vitorfer238@gmail.com>
2021-09-30 13:26:39 -0300

    first commit

---
+++ app/src/main/java/com/github/vitor238/hiltdependencyinjection/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.vitor238.hiltdependencyinjection
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
