4323537b01fd72f73735792a69f398b53d36c634
Vitor Ferreira <vitorfer238@gmail.com>
2021-09-30 14:05:46 -0300

    Getting Started

---
+++ app/src/main/java/com/github/vitor238/hiltdependencyinjection/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.github.vitor238.hiltdependencyinjection
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
