35e1bab27ebb419c94e0d119b59a458b0a990d3d
Vitor Ferreira <vitorfer238@gmail.com>
2021-10-04 12:24:58 -0300

    Modules, @Binds and @Provides

--- app/src/main/java/com/github/vitor238/hiltdependencyinjection/MainActivity.kt
+++ app/src/main/java/com/github/vitor238/hiltdependencyinjection/MainActivity.kt
@@ -3,8 +3,13 @@ package com.github.vitor238.hiltdependencyinjection
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -23,24 +28,51 @@ class MainActivity : AppCompatActivity() {
 class SomeClass
 @Inject
 constructor(
-    //private val someInterfaceImpl: SomeInterface,
+    private val someInterfaceImpl: SomeInterface,
     private val gson: Gson
 ) {
     fun doAThing(): String {
-        //return "Look I got: ${someInterfaceImpl.getAThing()}"
-        return "something"
+        return "Look I got: ${someInterfaceImpl.getAThing()}"
     }
 }
 
 class SomeInterfaceImpl
 @Inject
-constructor() : SomeInterface {
+constructor(
+    private val someDependency: String
+) : SomeInterface {
     override fun getAThing(): String {
-        return "A Thing"
+        return "A Thing $someDependency"
     }
 
 }
 
 interface SomeInterface {
     fun getAThing(): String
+}
+
+@InstallIn(SingletonComponent::class)
+@Module
+class MyModule {
+
+    @Singleton
+    @Provides
+    fun provideSomeString(): String {
+        return "some string"
+    }
+
+    @Singleton
+    @Provides
+    fun provideSomeInterface(
+        someString: String
+    ): SomeInterface {
+        return SomeInterfaceImpl(someString)
+    }
+
+    @Singleton
+    @Provides
+    fun provideGson(): Gson {
+        return Gson()
+    }
+
 }
